1de162ba84421ad660f8ce77d5f77ecd27bd279c
Tobithedev <showgame360@gmail.com>
2023-05-03 07:36:36 +0100

    feat: fixed the donate page

--- pages/donate.tsx
+++ pages/donate.tsx
@@ -1,16 +1,27 @@
 import Layout from "@/layouts/default"
-import { ReactElement } from "react"
+import { truncateAddress } from "@/utils/truncate"
+import { ReactElement, useState } from "react"
 
 
 export default function Donate() {
+    const [copiedText,setCopiedText] = useState("")
     return (
         <>
             <div className="bg-[linear-gradient(159.24deg,#191919_52.47%,#0B3D91_192.07%)] h-screen">
                 <div className="flex flex-col justify-center items-center h-screen">
-                    <div className="rounded-[10px] mx-0 py-5 px-5 text-white bg-white/30 border-2 border-white/[0.10] w-[779px] h-[fit]">
+                    <div className="rounded-[10px] mx-0 py-5 px-5 text-white bg-white/30 border-2 border-white/[0.10] w-[275px] md:w-[779px] h-[fit]">
                         <p className="text-lg font-medium mb-2">Support Our Project</p>
                         <p className="mb-2">You can donate to our Polygon wallet address:</p>
-                        <p className="text-lg font-medium mb-2">0x403ec707705766b1D340f6fC7162a0A344647771</p>
+                        <p className="cursor-pointer text-lg font-medium mb-2" onClick={()=> {
+                            navigator.clipboard.writeText("0x403ec707705766b1D340f6fC7162a0A344647771").then(
+                                () => {
+                                  setCopiedText("Copied")
+                                  setTimeout(()=> {
+                                    setCopiedText("")
+                                  }, 2000)
+                                },
+                              );
+                        }}>{copiedText ? copiedText : truncateAddress("0x403ec707705766b1D340f6fC7162a0A344647771")}</p>
                         <p className="mb-2">Or to our bank account:</p>
                         <p className="text-lg font-medium mb-2">Bank Name: Kuda Microfinance Bank</p>
                         <p className="text-lg font-medium mb-2">Account Name: Chukwuemeka Oluwatobi Uche</p>
