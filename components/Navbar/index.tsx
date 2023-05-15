f31d27d80f03393a66bbe3376d79a096240af9ed
Tobithedev <showgame360@gmail.com>
2023-05-15 23:06:39 +0100

    feat: fixed connect button on mobile

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -137,7 +137,8 @@ export const Navbar = () => {
                                 </div>
                                 {/* <Button text={currentAccount ? "Profile" : "Connect Wallet"} on_click={currentAccount ? () => router.push("/profile") : connectWallet} /> */}
                                 <div className="flex flex-row items-center gap-10">
-                                    <div className="flex flex-row items-center gap-1">
+                                    {/* @ts-ignore */}
+                                    <div onClick={cycleOpen} className="flex flex-row items-center gap-1">
                                         <Connect />
                                     </div>
                                 </div>
