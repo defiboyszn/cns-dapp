b839792716c5d3c198a9ea0268e19b7df4847022
Tobithedev <showgame360@gmail.com>
2023-05-15 22:45:58 +0100

    feat: fixed page

--- pages/dapp/dashboard.tsx
+++ pages/dapp/dashboard.tsx
@@ -2,19 +2,24 @@ import Layout from "@/layouts/default"
 import { ReactElement } from "react"
 
 const Dashboard = () => {
-    return (
-        <>
-        
-        </>
-    )
+  return (
+    <>
+      <div className="bg-cover h-full w-full" style={{
+        background: "linear-gradient(90deg,rgba(25,24,24,1.0),#005741)"
+      }}>
+        <div className="bg-[url('/images/bg.svg')] pt-40 pb-32 gap-10 h-full w-full flex flex-col justify-center items-center">
+        </div>
+      </div>
+    </>
+  )
 }
 
 
 
 Dashboard.getLayout = function getLayout(page: ReactElement) {
-    return (
-      <Layout>
-        {page}
-      </Layout>
-    )
-  }
+  return (
+    <Layout>
+      {page}
+    </Layout>
+  )
+}
